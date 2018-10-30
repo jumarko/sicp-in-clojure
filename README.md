f6ae982b08edfb879a79b56a53c88ba50840f03e
Juraj Martinka <jumarko@gmail.com>
2018-10-30 09:53:43 +0100

    Generate project skeleton.

---
+++ README.md
@@ -0,0 +1,12 @@
+# sicp-in-clojure
+
+Examples for the Structure and Interpreation of Computer Programs book written in Clojure.
+
+
+## Usage
+
+```
+lein repl
+...
+```
+
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject sicp-in-clojure "0.1.0-SNAPSHOT"
+  :description "Examples for the Structure and Interpreation of Computer Programs book written in Clojure"
+  :url "https://github.com/jumarko/sicp-in-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0-RC1"]]
+  :main ^:skip-aot sicp-in-clojure.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
