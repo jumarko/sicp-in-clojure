bda84e8b544d036ca4078a0967dcd59243ba0186
Juraj Martinka <jumarko@gmail.com>
2018-11-01 14:16:46 +0100

    First cut of sqrt fn using Netwon's method and naive `good-enough?` implementation.

--- src/sicp_in_clojure/01-abstractions-procedures/core.clj
+++ src/sicp_in_clojure/01-abstractions-procedures/core.clj
@@ -13,3 +13,40 @@
   (sum-of-squares (+ a 1) (* a 2)))
 
 (f 5)
+
+(defn abs [x]
+  (if (< x 0) (- x) x))
+(abs 10)
+(abs 0)
+(abs -10)
+
+;;; 1.1.7 Example: Square Roots by Newton's Method
+;; the first quite bad implementation of `good-enough?`
+;; it's bad because it doesn't really for for either small or large numbers
+(defn- good-enough? [guess x]
+  (< (abs (- (square guess)
+             x))
+     0.001))
+
+(defn- avg [x y]
+  (/ (+ x y) 2))
+
+(defn- improve [guess x]
+  (avg guess (/ x guess)))
+
+(defn- sqrt-iter [guess x]
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x)
+               x)))
+(defn sqrt [x]
+  (-> (sqrt-iter 1 x)
+      double))
+
+(sqrt 9)
+
+(sqrt (+ 100 37))
+
+(sqrt (+ (sqrt 2) (sqrt 3)))
+
+(square (sqrt 1000))
